dff8c2511a004aeda648979000041a2045c91789
Igor Ilic <gigili@users.noreply.github.com>
2021-01-09 17:44:54 +0100

    Add support for !drop

--- maximo.ts
+++ maximo.ts
@@ -10,6 +10,15 @@ client.on('message', (channel: string, tags: User, message: string, self: unknow
 	const getCommandFromMessage = message.split(" ")[0];
 	const getRestOfMessage = message.split(" ").slice(1);
 
+	if(message === "!drop" && tags.username.toLowerCase() === "gacbl"){
+		constemotes = ["Kappa", "KappaPride", "KappaRoss" "KappaWealth" "AppaK",""];
+		constemote = emotes[Math.floor(Math.random() * emotes.length)];
+		setTimeout(() => {
+			client.say(channel, "!drop");
+		}, 1500);
+		return;
+	}
+	
 	if (message && message.startsWith("!")) {
 		commandController.GetCommandName(
 			getCommandFromMessage,
