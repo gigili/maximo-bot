f7ed71b4f8e27b1cec2506cf5586ac0b68ed8e15
Igor Ilic <gigili@users.noreply.github.com>
2021-01-09 17:52:33 +0100

    Update maximo.ts

--- maximo.ts
+++ maximo.ts
@@ -10,8 +10,8 @@ client.on('message', (channel: string, tags: User, message: string, self: unknow
 	const getCommandFromMessage = message.split(" ")[0];
 	const getRestOfMessage = message.split(" ").slice(1);
 
-	if(message === "!drop" && tags.username.toLowerCase() === "gacbl"){
-		constemotes = ["Kappa", "KappaPride", "KappaRoss", "KappaWealth", "AppaK",""];
+	if(message.startsWith("!drop") && tags.username.toLowerCase() === "gacbl"){
+		constemotes = ["Kappa", "KappaPride", "KappaRoss", "KappaWealth", ""];
 		constemote = emotes[Math.floor(Math.random() * emotes.length)];
 		setTimeout(() => {
 			client.say(channel, `!drop ${emote}`);
