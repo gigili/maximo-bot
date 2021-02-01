bf4793c92f38deeb9947de9615ca306fca64adda
Ubuntu <ubuntu@ip-172-31-11-236.eu-north-1.compute.internal>
2021-02-01 12:57:02 +0000

    Fix merge

--- utility/config.ts
+++ utility/config.ts
@@ -1,11 +1,7 @@
 import {Config} from "./types";
 
 export const config: Config = {
-<<<<<<< HEAD
-	channels: ["gacbl", "thatn00b__"],
-=======
 	channels: ["gacbl", "thatn00b__", "gabbo1416"],
->>>>>>> 8a753bce0e134615bd323641404c3c10e483379a
 	debug: {
 		enabled: false,
 		url: "irc.fdgt.dev"
