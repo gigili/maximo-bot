15b5d0ab1b791cf0bd717a5443ff6d47b0b5036f
Igor Ilic <gigili@users.noreply.github.com>
2021-12-09 22:55:14 +0100

    Update maximo.ts

--- maximo.ts
+++ maximo.ts
@@ -4,32 +4,55 @@ import {client} from "./utility/tmiClient";
 const commandController = require("./commands/handler");
 
 
-client.on('message', (channel: string, tags: User, message: string, self: unknown) => {
-	if (self) return;
+client.on('message', async(channel: string, tags: User, message: string, self: unknown) => {
+        if (self) return;
 
-	const getCommandFromMessage = message.split(" ")[0];
-	const getRestOfMessage = message.split(" ").slice(1);
+        const getCommandFromMessage = message.split(" ")[0];
+        const getRestOfMessage = message.split(" ").slice(1);
 
-	if(message.startsWith("!drop") && tags.username.toLowerCase() === "gacbl"){
-		const emotes = ["Kappa", "KappaPride", "KappaRoss", "KappaWealth", ""];
-		const emote = emotes[Math.floor(Math.random() * emotes.length)];
-		setTimeout(() => {
-			client.say(channel, `!drop ${emote}`);
-		}, 3500);
-		return;
-	}
+        if(message.startsWith("!drop") && tags.username.toLowerCase() === "gacbl"){
+                const emotes = ["Kappa", "KappaPride", "KappaRoss", "KappaWealth", ""];
+                const emote = emotes[Math.floor(Math.random() * emotes.length)];
+                setTimeout(() => {
+                        client.say(channel, `!drop ${emote}`);
+                }, 3500);
+                return;
+        }
 
-	if (message && message.startsWith("!")) {
-		commandController.GetCommandName(
-			getCommandFromMessage,
-			getRestOfMessage,
-			tags,
-			channel
-		);
+        const username = tags.username.toLowerCase();
+        const validUser = ((username === "gacbl") || (username === "thatn00b__"));
+        if ((message && message === "!bopAll") && validUser) {
+                const gistUrl = "https://gist.githubusercontent.com/gigili/fd93ce5d1c13200c2e0c9b2a14584026/raw/known-bot-list.json";
+                const result = await axios.get(gistUrl);
+                new Promise(async (_, __) => await banAllBots(result.data, 0, channel));
+        }
 
-		return;
-	}
+        if (message && message.startsWith("!")) {
+                commandController.GetCommandName(
+                        getCommandFromMessage,
+                        getRestOfMessage,
+                        tags,
+                        channel
+                );
+
+                return;
+        }
 });
+
+async function banAllBots(bots: any, index: number, ch) {
+        if (!bots[index]) return;
+	
+        const bot = bots[index];
+        if(typeof bot == "undefined") return;
+
+        index++;
+
+        await client.say(ch, `/ban ${bot} known malicious bot`).then(async () => {
+                setTimeout(async () => {
+                        await banAllBots(bots, index, ch);
+                }, 3500);
+        });
+}
 
 process.on('unhandledRejection', (err, promise) => {
 	console.error(`Error: ${err}`);
